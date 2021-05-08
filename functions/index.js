f3c42e6ddf2dd37d22483dff83b9171d5baaccf2
Uros Randelovic <uros.randelovic@gmail.com>
2021-05-07 21:20:41 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,5 @@
-const functions = require("firebase-functions");
+const user_functions = require('./js_functions/user_functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Export Your First Cloud Functions
+
+exports.templateFunction = user_functions.templateFunction;
